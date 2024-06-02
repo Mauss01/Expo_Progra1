faa52ff81bf9ce5e69d96fed07cfe0d700cf640c
luisfigueroa2 <167578006+luisfigueroa2@users.noreply.github.com>
2024-06-02 17:09:36 -0600

    Update Libro.java

--- src/expo/progra1/Libro.java
+++ src/expo/progra1/Libro.java
@@ -8,6 +8,39 @@ package expo.progra1;
  *
  * @author gabri
  */
-class Libro {
-    
+public class Libro {
+private String titulo;
+    private String state;
+
+    public Libro(String titulo) {
+        this.titulo = titulo;
+        this.state = "disponible";
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getEstado() {
+        return state;
+    }
+
+    public void rentar() {
+        if (state.equals("disponible")) {
+            state = "prestado";
+            System.out.println("Rentaste: " + titulo);
+        } else {
+            System.out.println("Ya esta rentado.");
+        }
+    }
+
+    public void devolver() {
+        state = "disponible";
+        System.out.println("Devolviste: " + titulo);
+    }
+
+    @Override
+    public String toString() {
+        return titulo + " (" + state + ")";
+    }    
 }
